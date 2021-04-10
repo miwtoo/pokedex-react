ef22b76abb983b8a8143e69b8e90692158aaf553
Miwtoo <miwyugioh@gmail.com>
2021-04-10 22:06:00 +0700

    feat: :sparkles: can show image and type in card

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Card from "./component/Card";
 function App() {
   const [pokemons, setPokemon] = useState({} as Pokemons);
   useEffect(() => {
-    axios.get(" https://pokeapi.co/api/v2/pokemon?limit=10").then((res) => {
+    axios.get(" https://pokeapi.co/api/v2/pokemon?limit=151").then((res) => {
       setPokemon(res.data as Pokemons);
     });
   }, []);
@@ -14,11 +14,7 @@ function App() {
   return (
     <div>
       {pokemons?.results?.map((pokemon) => {
-        return (
-          <a href={pokemon.url} className="my-6">
-            <Card text={pokemon.name}></Card>
-          </a>
-        );
+        return <Card pokemon={pokemon} key={pokemon.name}></Card>;
       })}
     </div>
   );
--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,7 +1,62 @@
-export default function Card(props: { text: string }) {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./card.css";
+
+export default function Card(props: {
+  pokemon: { name: string; url: string };
+}) {
+  const [pokemon, setPokemon] = useState({} as Pokemon);
+  useEffect(() => {
+    axios.get(props.pokemon.url).then((res) => {
+      setPokemon(res.data);
+    });
+  }, [props.pokemon.url]);
   return (
-    <div className="py-8 px-8 my-4 shadow-xl bg-white bg-opacity-50 rounded-2xl">
-      {props.text}
+    <div className="my-6 mx-4 shadow-xl bg-white bg-opacity-50 rounded-2xl">
+      <div className="grid grid-cols-2">
+        <div className="bg-gray-300 bg-opacity-90 rounded-2xl">
+          <div className="p-6">
+            <div>{oneCapital(props.pokemon.name)}</div>
+            <div>
+              {pokemon?.types?.map((v) => (
+                <span className={"rounded p-1 mr-1 bg-white"}>
+                  {v.type.name}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="p-6">
+          <img
+            className="filter drop-shadow-lg"
+            src={pokemon.sprites?.other?.dream_world?.front_default}
+            alt={props.pokemon.name}
+          />
+        </div>
+      </div>
     </div>
   );
 }
+
+function oneCapital(text: string) {
+  return text[0].toUpperCase() + text.slice(1);
+}
+
+type Pokemon = {
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string;
+        "official-artwork": string;
+      };
+    };
+  };
+  types: [
+    {
+      slot: number;
+      type: {
+        name: string;
+      };
+    }
+  ];
+};
